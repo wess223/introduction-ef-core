5a852b4a2882dabe93fcfae9549a3ca3fb7a50bc
Wesley Alves <wesley.info07@gmail.com>
2022-09-16 10:28:54 -0300

    dbContext applyed configurations.

--- EntroductionEF/Data/DataContext.cs
+++ EntroductionEF/Data/DataContext.cs
@@ -1,4 +1,5 @@
-﻿using EntroductionEF.Models;
+﻿using EntroductionEF.Data.Configurations;
+using EntroductionEF.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace EntroductionEF.Data
@@ -12,53 +13,10 @@ namespace EntroductionEF.Data
             options.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb;Integrated Security=True");
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
         {
-            modelBuilder.Entity<Client>(p =>
-            {
-                p.ToTable("Clients");
-                p.HasKey(p => p.Id);
-                p.Property(p => p.Name).HasColumnType("VARCHAR(80)").IsRequired();
-                p.Property(p => p.Telephone).HasColumnType("CHAR(11)").IsRequired();
-                p.Property(p => p.Cep).HasColumnType("CHAR(8)").IsRequired();
-                p.Property(p => p.State).HasColumnType("CHAR(2)").IsRequired();
-                p.Property(P => P.City).HasMaxLength(60).IsRequired();
-
-                p.HasIndex(i => i.Telephone).HasDatabaseName("idx_client_telephone");
-            });
-
-            modelBuilder.Entity<Product>(p =>
-            {
-                p.ToTable("Products");
-                p.HasKey(p => p.Id);
-                p.Property(p => p.BarCode).HasColumnType("VARCHAR(14)").IsRequired();
-                p.Property(p => p.Description).HasColumnType("VARCHAR(60)");
-                p.Property(p => p.Value).IsRequired();
-                p.Property(p => p.TypeProduct).HasConversion<string>();
-            });
-
-            modelBuilder.Entity<Solicitation>(p =>
-            {
-                p.ToTable("Solicitations");
-                p.HasKey(p => p.Id);
-                p.Property(p => p.StartDate).HasDefaultValueSql("GETDATE()").ValueGeneratedOnAdd();
-                p.Property(p => p.Status).HasConversion<string>();
-                p.Property(p => p.TypeShipping).HasConversion<int>();
-                p.Property(p => p.Observation).HasColumnType("VARCHAR()512");
-
-                p.HasMany(p => p.ItemList)
-                .WithOne(p => p.Solicitation)
-                .OnDelete(DeleteBehavior.Cascade);
-            });
-
-            modelBuilder.Entity<SolicitationItem>(p =>
-            {
-                p.ToTable("SolicitationItems");
-                p.HasKey(p => p.Id);
-                p.Property(p => p.Quantity).HasDefaultValue(1).IsRequired();
-                p.Property(p => p.Value).IsRequired();
-                p.Property(p => p.Discount).IsRequired();
-            });
+            //Aplique a configuração para todas as classes concretas que estão implementando IEntityTypeConfiguration nesse assembly.
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
         }
 
 
